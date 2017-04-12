9b341448274db7c2bd33c5d3eaa77fa45675ca56
maeganjwilson <maegan@maeganwilson.com>
2017-04-12 14:45:13 -0500

    Adding tips and functino

--- js/cards.js
+++ js/cards.js
@@ -9,7 +9,10 @@ function newTip(ttl, dscrp, tg) {
     };
     cardList.push(cardObj);
 }
+function printCards(){
+
+}
 
 newTip('When to use FixtureNet?', 'FixtureNet is used to connect media servers, ArtNet nodes, E1.31(SACN), video switchers, and Visualizers.', 'networking'
 );
-newTip('When to use HogNet','FixtureNet is used to connect media servers, ArtNet nodes, E1.31(SACN), video switchers, and Visualizers.', 'networking');
+newTip('When to use HogNet','HogNet is used to connect DP 8000\'s, OSC (Open Sound Control), and backup/tracking consoles.', 'networking');
