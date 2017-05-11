54874a06a1e3e0f66048929f60f3b90da6d97c6e
maeganjwilson <maegan@maeganwilson.com>
2017-05-10 19:45:43 -0500

    Event listeners kinda work. Need to be able to show all cards

---
+++ app-min.js
@@ -0,0 +1,1 @@
+$(".card-tag").on("click",".js-prog",function(){$(".card").addClass("dontShow"),$(".prog").removeClass("dontShow")}),$(".card-tag").on("click",".js-net",function(){$(".card").addClass("dontShow"),$(".net").removeClass("dontShow")}),$(".card-tag").on("click",".js-play",function(){$(".card").addClass("dontShow"),$(".play").removeClass("dontShow")});
