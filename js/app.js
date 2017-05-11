39e4af9d0e61891402c29816fed60fbfb50aa7eb
maeganjwilson <maegan@maeganwilson.com>
2017-05-10 20:20:25 -0500

    Successfully shows all and only shows some

---
+++ js/app.js
@@ -0,0 +1,38 @@
+function hide(select){
+  select.addClass('dontShow');
+}
+function show(select){
+  select.removeClass('dontShow');
+}
+function showButton() {
+  $('.js-showAll').removeClass('dontShow');
+}
+function hideButton() {
+  $('.js-showAll').addClass('dontShow');
+
+}
+
+/*=============================================>>>>>
+= Shows and hides as needed =
+===============================================>>>>>*/
+$('.card-tag').on('click', '.js-prog', function (){
+  hide($('.card'));
+  show($('.prog'));
+  showButton();
+});
+$('.card-tag').on('click', '.js-net', function (){
+  hide($('.card'));
+  show($('.net'));
+  showButton();
+});
+$('.card-tag').on('click', '.js-play', function (){
+  hide($('.card'));
+  show($('.play'));
+  showButton();
+});
+$('.js-showAll').on('click', function(){
+  hideButton();
+  show($('.card'));
+});
+/*= End of Shows and hides as needed =*/
+/*=============================================<<<<<*/
