225630d03c602285290f69511d46f7a5dccff29c
maeganjwilson <maegan@maeganwilson.com>
2017-05-31 09:24:54 -0500

    Working on beautify

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
+/* eslint-env jquery */
 /*=============================================>>>>>
 = show and hide =
 ===============================================>>>>>*/
 
 function hide(select){
-  select.addClass('dontShow');
+    select.addClass('dontShow');
 }
 function show(select){
-  select.removeClass('dontShow');
+    select.removeClass('dontShow');
 }
 function showButton() {
-  $('.js-showAll').removeClass('dontShow');
+    $('.js-showAll').removeClass('dontShow');
 }
 function hideButton() {
-  $('.js-showAll').addClass('dontShow');
+    $('.js-showAll').addClass('dontShow');
 }
 
 /*= End of show and hide =*/
@@ -22,41 +23,41 @@ function hideButton() {
 = Shows and hides as needed =
 ===============================================>>>>>*/
 $('.card-tag').on('click', '.js-prog', function (){
-  hide($('.card'));
-  show($('.prog'));
-  showButton();
+    hide($('.card'));
+    show($('.prog'));
+    showButton();
 });
 $('.card-tag').on('click', '.js-net', function (){
-  hide($('.card'));
-  show($('.net'));
-  showButton();
+    hide($('.card'));
+    show($('.net'));
+    showButton();
 });
 $('.card-tag').on('click', '.js-play', function (){
-  hide($('.card'));
-  show($('.play'));
-  showButton();
+    hide($('.card'));
+    show($('.play'));
+    showButton();
 });
 
 $('.js-prog').on('click', function (){
-  hide($('.card'));
-  show($('.prog'));
-  showButton();
+    hide($('.card'));
+    show($('.prog'));
+    showButton();
 });
 $('.js-net').on('click', function (){
-  hide($('.card'));
-  show($('.net'));
-  showButton();
+    hide($('.card'));
+    show($('.net'));
+    showButton();
 });
 $('.js-play').on('click', function (){
-  hide($('.card'));
-  show($('.play'));
-  showButton();
+    hide($('.card'));
+    show($('.play'));
+    showButton();
 });
 
 
 $('.js-showAll').on('click', function(){
-  hideButton();
-  show($('.card'));
+    hideButton();
+    show($('.card'));
 });
 /*= End of Shows and hides as needed =*/
 /*=============================================<<<<<*/
